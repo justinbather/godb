b9bfd0af6e84605438ec1a0108157868cc7b3749
justinbather <justinbather@hotmail.com>
2023-12-15 19:11:19 -0500

    refactor to map store

--- schema.go
+++ schema.go
@@ -1,26 +1,46 @@
 package godb
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 )
 
-type MemStore struct {
-	Name   string
-	Stores map[string]StoreSchema
+type Store struct {
+	mu   sync.RWMutex
+	data map[string][]byte
 }
 
-var MemDb *MemStore
-
-type StoreSchema interface{}
-
-func (ms *MemStore) NewStore(name string, s interface) {
-
-  store := &StoreSchema
-
+func NewStorer() *Store {
+	store := &Store{data: make(map[string][]byte)}
+	return store
 }
 
-func Init(name string) *MemStore {
-	MemDb = &MemStore{Name: name}
-	fmt.Println("New MemDb created")
-	return MemDb
+func (s *Store) ListContents() {
+	for key, val := range s.data {
+		fmt.Println(key, val)
+	}
+}
+
+func (s *Store) Get(k string) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	value, ok := s.data[k]
+
+	if !ok {
+		err := errors.New("Error: No value found")
+		return nil, err
+	}
+
+	return value, nil
+}
+
+func (s *Store) Set(k string, v []byte) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.data[k] = v
+
+	return nil
 }
--- schema_test.go
+++ schema_test.go
@@ -1,13 +1,32 @@
 package godb
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestCreateSchema(t *testing.T) {
+var store *Store
 
+func TestCreateStore(t *testing.T) {
+
+	store = NewStorer()
 }
 
-func TestInit(t *testing.T) {
+func TestSet(t *testing.T) {
+	data := []byte{'i'}
+	store.Set("test", data)
+}
 
+func TestGet(t *testing.T) {
+	data, err := store.Get("test")
+
+	if err != nil {
+		t.Fatalf("err %s", err)
+	}
+
+	fmt.Println(data)
+}
+
+func TestPrint(t *testing.T) {
+	store.ListContents()
 }
