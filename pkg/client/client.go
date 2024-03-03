1903b1e75aca2f05c418115ed41ab24ac9c471cb
Justin Bather <justinbather@hotmail.com>
2024-03-03 16:04:09 -0500

    feat: add connect func

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,5 +1,9 @@
 package client
 
+import (
+	"net/http"
+)
+
 func Get(cfg *Config, key string) (interface{}, error) {
 
 	return nil, nil
@@ -16,7 +20,13 @@ func Delete(cfg *Config, key string) error {
 }
 
 // User should run this to ensure the config is correct and docker server is running, ready to be used
-func Connect() error {
+func Connect(cfg *Config) error {
+
+	// Just need a simple ping to the server to ensure its running and the users config is correct
+	_, err := http.Get(cfg.Address)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
